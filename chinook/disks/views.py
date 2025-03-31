31b0567a0855ec930003cb9db43e3041a889a638
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-03-31 16:35:40 +0200

    Mise en place recherche album

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Album, Track
 
 def album_list(request):
-    """Affiche la liste de tous les albums"""
-    albums = Album.objects.all()
-    return render(request, 'disks/album_list.html', {'albums': albums})
+    """Affiche la liste des albums, avec un champ de recherche"""
+    query = request.GET.get('q', '')  
+    if query:
+        albums = Album.objects.filter(title__icontains=query) 
+    else:
+        albums = Album.objects.all()  
+
+    return render(request, 'disks/album_list.html', {'albums': albums, 'query': query})
 
 def album_detail(request, album_id):
     """Affiche les morceaux d'un album avec les détails"""

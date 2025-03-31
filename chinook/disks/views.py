3b0a4b26c833996829c6184211f1dd3bede62c8d
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-03-31 16:54:43 +0200

    recherche élargie au titre et noms d'artiste

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Track
+from django.db.models import Q
 
 def album_list(request):
-    """Affiche la liste des albums, avec un champ de recherche"""
-    query = request.GET.get('q', '')  
+    """Affiche la liste des albums, avec un champ de recherche élargi"""
+    query = request.GET.get('q', '')
+
     if query:
-        albums = Album.objects.filter(title__icontains=query) 
+        albums = Album.objects.filter(
+            Q(title__icontains=query) | Q(artist__name__icontains=query)
+        )
     else:
-        albums = Album.objects.all()  
+        albums = Album.objects.all()
 
     return render(request, 'disks/album_list.html', {'albums': albums, 'query': query})
+
 
 def album_detail(request, album_id):
     """Affiche les morceaux d'un album avec les détails"""

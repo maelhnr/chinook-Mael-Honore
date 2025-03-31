fa466ed38c034e1fe7e7d6a3634d95cb20da924e
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-03-31 14:59:58 +0200

    Définition des modèles Track, Album et Artist

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class Track(models.Model):
+    name = models.CharField(max_length=255)
+    composer = models.CharField(max_length=255, null=True, blank=True)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField()
+    unitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
